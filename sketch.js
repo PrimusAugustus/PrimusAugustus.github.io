f4aa3d52532a4146e0f86b31ad5b2a38f014cbbf
PrimusAugustus <114185959+PrimusAugustus@users.noreply.github.com>
2022-09-23 08:30:57 +0200

    Add basic Website files
    Für die Informatik-Website Stunde 2

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  canvasX = 600;
+  canvasY = 387;
+  createCanvas(canvasX, canvasY);
+  
+  r = 25;
+  x = r;
+  y = r;
+  SpeedX = 3;
+  SpeedY = 3;
+}
+
+function draw() {
+  background(220,50,150);
+  if(x > canvasX - r||x < r)SpeedX *= -1;
+  if(y > canvasY - r||y < r)Speedy *= -1;
+  
+  x += SpeedX;
+  y += SpeedY;
+
+  circle(x,y,r);
+
+}
